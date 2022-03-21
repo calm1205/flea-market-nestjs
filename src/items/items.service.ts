f32b2ccda2b64d2c54fac46dfe613e061a616fec
calm1205 <naginani072@gmail.com>
2022-03-21 10:43:20 +0900

    update id to uuid

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { ItemStatus } from './item-status.enum';
 import { Item } from './item.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class ItemsService {
@@ -17,6 +18,7 @@ export class ItemsService {
 
   create(createItemDto: CreateItemDto): Item {
     const item: Item = {
+      id: uuid(),
       ...createItemDto,
       status: ItemStatus.ON_SALE,
     };
