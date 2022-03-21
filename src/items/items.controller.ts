75f393e0d3c720e379bd1f4a551a7f6afbfcf344
calm1205 <naginani072@gmail.com>
2022-03-21 10:24:28 +0900

    define findById

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ItemStatus } from './item-status.enum';
 import { Item } from './item.model';
 import { ItemsService } from './items.service';
@@ -10,6 +10,11 @@ export class ItemsController {
   @Get()
   findAll(): Item[] {
     return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string): Item {
+    return this.itemsService.findById(id);
   }
 
   @Post()
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -9,6 +9,10 @@ export class ItemsService {
     return this.items;
   }
 
+  findById(id: string): Item {
+    return this.items.find((item) => item.id === id);
+  }
+
   create(item: Item): Item {
     this.items.push(item);
     return item;
