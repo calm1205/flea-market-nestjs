52bb3639a94a5e4d9a54f34bccb0489860ec4a51
calm1205 <naginani072@gmail.com>
2022-03-21 09:29:37 +0900

    nest g module items

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [ItemsModule],
   controllers: [],
   providers: [],
 })
