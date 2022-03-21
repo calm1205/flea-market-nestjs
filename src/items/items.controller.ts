590f23d74ebe58e7292d128c6baef534b462420e
calm1205 <naginani072@gmail.com>
2022-03-21 10:10:28 +0900

    define create items routing

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ItemStatus } from './item-status.enum';
+import { Item } from './item.model';
 import { ItemsService } from './items.service';
 
 @Controller('items')
@@ -8,5 +10,22 @@ export class ItemsController {
   @Get()
   findAll() {
     return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+    return this.itemsService.create(item);
   }
 }
