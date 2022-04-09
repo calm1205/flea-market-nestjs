824850d73cba873369fe19810718cfefa267b64e
calm1205 <naginani072@gmail.com>
2022-04-09 16:14:37 +0900

    jwtの初期設定

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { UserRepository } from './user.repository';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'secret_key_of_this_app', // シークレットキー
+      signOptions: {
+        expiresIn: 3600, // 期限,単位は秒
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
